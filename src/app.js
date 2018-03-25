ca4ebcabf27dac454da995b5d4eeef9cf20a7cd2
Jayan <jayanpatel@Jayans-MBP.home>
2018-03-25 14:55:07 -0400

    Adding buildpack to Heroku

--- src/app.js
+++ src/app.js
@@ -1,35 +1,36 @@
-//Install express server
-const express = require('express');
-const app = express();
+// //Install express server
+// const express = require('express');
+// const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '../dist'));
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
 
+console.log("Hello World!");
 
-$(document).ready(function(){
-	// function initMap() {
-	// 	var uluru = {lat: -25.363, lng: 131.044};
-	// 	var map = new google.maps.Map(document.getElementById('map'), {
-	// 	  zoom: 4,
-	// 	  center: uluru
-	// 	});
-	// 	var marker = new google.maps.Marker({
-	// 	  position: uluru,
-	// 	  map: map
-	// 	});
-	// }
-	console.log($('#map'));
-});
+// $(document).ready(function(){
+// 	// function initMap() {
+// 	// 	var uluru = {lat: -25.363, lng: 131.044};
+// 	// 	var map = new google.maps.Map(document.getElementById('map'), {
+// 	// 	  zoom: 4,
+// 	// 	  center: uluru
+// 	// 	});
+// 	// 	var marker = new google.maps.Marker({
+// 	// 	  position: uluru,
+// 	// 	  map: map
+// 	// 	});
+// 	// }
+// 	console.log($('#map'));
+// });
 
-document.onload(
-	$('nav div a.nav-link').click(
-		function() { 
-			$('nav div a.active').removeClass("active"); 
-			$(this).addClass("active"); 
-		}
-	);
-);
+// document.onload(
+// 	$('nav div a.nav-link').click(
+// 		function() { 
+// 			$('nav div a.active').removeClass("active"); 
+// 			$(this).addClass("active"); 
+// 		}
+// 	);
+// );
 
